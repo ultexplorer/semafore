2671ebd2a613530c76c448ed6c8fce324f40916c
ultexplorer <ultexplorer@gmail.com>
2023-01-03 11:07:59 +0200

    v2

--- 2-binary.js
+++ 2-binary.js
@@ -9,14 +9,14 @@ class BinarySemaphore {
     constructor(shared, offset = 0, init = false){
         this.lock = new Int8Array(shared, offset, 1);
         if(init) this.lock[0] = UNLOCKED;
-    };
+    }
     enter(){
         while(this.lock[0] !== UNLOCKED);
         this.lock[0] = LOCKED;
     }
     leave(){
         if(this.lock[0] === UNLOCKED){
-            throw new Error('Cannot leave unlocked BinarySemaphore');
+            throw new Error('cannot leave unlocked semaphore');
         }
         this.lock[0] = UNLOCKED;
     }
@@ -28,17 +28,17 @@ if(isMainThread){
     console.dir({ semaphore });
     new Worker(__filename, { workerData: buffer });
     new Worker(__filename, { workerData: buffer });
-}else{
-    const { threadId, workerData } = threads;
+}else {
+    const {threadId, workerData} = threads;
     const semaphore = new BinarySemaphore(workerData);
     const array = new Int8Array(workerData, 1);
     semaphore.enter();
     setInterval(() => {
         let value = threadId === 1 ? 1 : -1;
-        for(let i = 0; i < 10; i++){
-            array[i] += value;
+        for (let i = 0; i < 10; i++) {
+            array[0] += value;
         }
-        console.dir([ threadId, array ]);
+        console.dir([threadId, array]);
         semaphore.leave();
-    }, 100);
+    }, 100)
 }
