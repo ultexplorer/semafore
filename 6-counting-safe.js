c56ecc691c29153db125936c30614b1bd34653fb
ultexplorer <ultexplorer@gmail.com>
2023-01-05 11:25:10 +0200

    Добавил 7-binary.js и 8-await-counting.js

---
+++ 6-counting-safe.js
@@ -0,0 +1,49 @@
+'use strict';
+const fs = require('fs');
+const threads = require('worker_threads');
+const { Worker, isMainThread } = threads;
+
+class CountingSemaphore {
+    constructor(shared, offset = 0, initial){
+        this.counter = new Int32Array(shared, offset, 1);
+        if(typeof initial === 'number'){
+            Atomics.store(this.counter, 0, initial);
+        }
+    }
+
+    enter(){
+        while(true){
+            Atomics.wait(this.counter, 0, 0);
+            const n = Atomics.load(this.counter, 0);
+            if(n > 0){
+                const prev = Atomics.compareExchange(this.counter, 0, n, n-1);
+                if(prev === n) return;
+            }
+        }
+    }
+
+    leave(){
+        Atomics.add(this.counter, 0, 1);
+        Atomics.notify(this.counter, 0, 1);
+    }
+}
+
+if(isMainThread){
+    const buffer = new SharedArrayBuffer(4);
+    const semaphore = new CountingSemaphore(buffer, 0, 10);
+    console.dir({ semaphore: semaphore.counter[0 ]});
+    for(let i = 0; i < 20; i++){
+        new Worker(__filename, { workerData: buffer });
+    }
+}else{
+    const { threadId, workerData } = threads;
+    const semaphore = new CountingSemaphore(workerData);
+    const REPEAT_COUNT = 1000000;
+    const file = `file-${threadId}.dat`;
+    console.dir({ threadId, semaphore: semaphore.counter[0] });
+    semaphore.enter();
+    const data = `Data from file-${threadId}`.repeat(REPEAT_COUNT);
+    fs.writeFile(file, data, () => {
+        fs.unlink(file, () => semaphore.leave());
+    })
+}
